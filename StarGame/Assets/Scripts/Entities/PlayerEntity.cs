653380b1384b152926443715539aff5ba0a3caf1
KentPhan <u0734504@ad.utah.edu>
2018-09-26 21:20:22 -0600

    Added initial gyroscope controls. Modified build settings for android

--- StarGame/Assets/Scripts/Entities/PlayerEntity.cs
+++ StarGame/Assets/Scripts/Entities/PlayerEntity.cs
@@ -9,13 +9,16 @@ public class PlayerEntity : Entity
 {
 
 
+    public enum ControllerScheme { Mouse = 0, Gyroscope = 1 }
 
+    public ControllerScheme control = ControllerScheme.Mouse;
 
     public enum RotationAxes { MouseXAndY = 0, MouseX = 1, MouseY = 2 }
     public RotationAxes axes = RotationAxes.MouseXAndY;
 
     public CursorLockMode wantedMode;
 
+    // Mouse
     public float sensitivityX = 15F;
     public float sensitivityY = 15F;
     public float minimumX = -360F;
@@ -26,39 +29,67 @@ public class PlayerEntity : Entity
     float rotationY = 0F;
     Quaternion originalRotation;
 
+
+    //Gyroscope
+    private Gyroscope m_Gyro;
+
     void Start()
     {
         Cursor.lockState = wantedMode;
+
         originalRotation = transform.localRotation;
+
+        m_Gyro = Input.gyro;
+        m_Gyro.enabled = true;
+    }
+
+    void OnGUI()
+    {
+
     }
 
     void Update()
     {
-        if (axes == RotationAxes.MouseXAndY)
+        if (control == ControllerScheme.Mouse)
         {
-            // Read the mouse input axis
-            rotationX += Input.GetAxis("Mouse X") * sensitivityX;
-            rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
-            rotationX = ClampAngle(rotationX, minimumX, maximumX);
-            rotationY = ClampAngle(rotationY, minimumY, maximumY);
-            Quaternion xQuaternion = Quaternion.AngleAxis(rotationX, Vector3.up);
-            Quaternion yQuaternion = Quaternion.AngleAxis(rotationY, -Vector3.right);
-            transform.localRotation = originalRotation * xQuaternion * yQuaternion;
-        }
-        else if (axes == RotationAxes.MouseX)
-        {
-            rotationX += Input.GetAxis("Mouse X") * sensitivityX;
-            rotationX = ClampAngle(rotationX, minimumX, maximumX);
-            Quaternion xQuaternion = Quaternion.AngleAxis(rotationX, Vector3.up);
-            transform.localRotation = originalRotation * xQuaternion;
+            if (axes == RotationAxes.MouseXAndY)
+            {
+                // Read the mouse input axis
+                rotationX += Input.GetAxis("Mouse X") * sensitivityX;
+                rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+                rotationX = ClampAngle(rotationX, minimumX, maximumX);
+                rotationY = ClampAngle(rotationY, minimumY, maximumY);
+                Quaternion xQuaternion = Quaternion.AngleAxis(rotationX, Vector3.up);
+                Quaternion yQuaternion = Quaternion.AngleAxis(rotationY, -Vector3.right);
+                transform.localRotation = originalRotation * xQuaternion * yQuaternion;
+            }
+            else if (axes == RotationAxes.MouseX)
+            {
+                rotationX += Input.GetAxis("Mouse X") * sensitivityX;
+                rotationX = ClampAngle(rotationX, minimumX, maximumX);
+                Quaternion xQuaternion = Quaternion.AngleAxis(rotationX, Vector3.up);
+                transform.localRotation = originalRotation * xQuaternion;
+            }
+            else
+            {
+                rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+                rotationY = ClampAngle(rotationY, minimumY, maximumY);
+                Quaternion yQuaternion = Quaternion.AngleAxis(-rotationY, Vector3.right);
+                transform.localRotation = originalRotation * yQuaternion;
+            }
         }
         else
         {
-            rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
-            rotationY = ClampAngle(rotationY, minimumY, maximumY);
-            Quaternion yQuaternion = Quaternion.AngleAxis(-rotationY, Vector3.right);
-            transform.localRotation = originalRotation * yQuaternion;
+            //rotationX += Input.gyro.rotationRateUnbiased.x * sensitivityX;
+            //rotationY += Input.gyro.rotationRateUnbiased.y * sensitivityY;
+            //rotationX = ClampAngle(rotationX, minimumX, maximumX);
+            //rotationY = ClampAngle(rotationY, minimumY, maximumY);
+            //Quaternion xQuaternion = Quaternion.AngleAxis(rotationX, Vector3.up);
+            //Quaternion yQuaternion = Quaternion.AngleAxis(-rotationY, Vector3.right);
+            //transform.localRotation = originalRotation * xQuaternion * yQuaternion;
+            transform.Rotate(-Input.gyro.rotationRateUnbiased.x, -Input.gyro.rotationRateUnbiased.y, 0);
         }
+
     }
 
     public static float ClampAngle(float angle, float min, float max)

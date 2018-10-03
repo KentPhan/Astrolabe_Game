2098da2f39e9b876407f0110255600bbb8aa26e5
Junhao Fu <jfu@sva.edu>
2018-10-02 20:10:46 -0600

    Add the Startscreen Animation, in JunhaoScene

---
+++ StarGame/Assets/Source/StartSceen/StarsRotation.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class StarsRotation : MonoBehaviour {
+
+    public float speed = 5;
+    public float Opacity = 1;
+
+    // Use this for initialization
+    void Start () {
+        GetComponent<Image>().color = new Color(1, 1, 1, Opacity);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.rotation *= Quaternion.Euler(0, 0, Time.deltaTime * -speed);
+        
+	}
+}
